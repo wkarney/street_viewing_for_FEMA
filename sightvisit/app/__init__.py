5e97a0274692c4c0c9f52895cd22456c7bf7435e
Will Karnasiewicz <willkarnasiewicz@gmail.com>
2019-04-24 20:17:36 -0400

    Revised upload path in init to relative path name

--- sightvisit/app/__init__.py
+++ sightvisit/app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['UPLOADED_PHOTOS_DEST'] = '/Users/orah82/streetview_2/app/static/img'
+app.config['UPLOADED_PHOTOS_DEST'] = 'app/img'
 
 
 
